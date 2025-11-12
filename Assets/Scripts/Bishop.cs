a8090b238d014de889183bf8e97be38891a71883
David Madsen <Dnm070902@gmail.com>
2025-11-12 07:46:20 +0100

    Added moves for bishops

--- Assets/Scripts/Bishop.cs
+++ Assets/Scripts/Bishop.cs
@@ -10,9 +10,127 @@ public class Bishop : ChessPiece
     public override List<(int, int)> GetPossibleMoves(Dictionary<(int, int), ChessPiece> pieceCoordsMap)
     {
         var possibleMoves = new List<(int, int)> { };
-        var prePossibleMoves = new List<(int, int)> { };
+        
+        bool topLeftBlocked = false;
+        bool topRightBlocked = false;
+        bool bottomLeftBlocked = false;
+        bool bottomRightBlocked = false;
 
-        throw new NotImplementedException();
+        for (int i = 1; i <= 7; i++)
+        {
+            // Top left diagonal moves
+            if (CurrentTilePosition.Item1 - i >= 0 && CurrentTilePosition.Item1 - i <= 7 && CurrentTilePosition.Item2 + i >= 0 && CurrentTilePosition.Item2 + i <= 7)
+            {
+                var topLeftPiece = pieceCoordsMap[(CurrentTilePosition.Item1 - i, CurrentTilePosition.Item2 + i)];
+                if (topLeftPiece != null && !topLeftBlocked)
+                {
+                    if (topLeftPiece.PieceColor != PieceColor)
+                    {
+                        possibleMoves.Add((CurrentTilePosition.Item1 - i, CurrentTilePosition.Item2 + i));
+                        topLeftBlocked = true;
+                    }
+                    else
+                    {
+                        topLeftBlocked = true;
+                    }
+                }
+                else if (!topLeftBlocked)
+                {
+                    possibleMoves.Add((CurrentTilePosition.Item1 - i, CurrentTilePosition.Item2 + i));
+                }
+            }
+            else
+            {
+                topLeftBlocked = true;
+            }
+
+            // Top right diagonal moves
+            if (CurrentTilePosition.Item1 + i >= 0 && CurrentTilePosition.Item1 + i <= 7 && CurrentTilePosition.Item2 + i >= 0 && CurrentTilePosition.Item2 + i <= 7)
+            {
+                var topRightPiece = pieceCoordsMap[(CurrentTilePosition.Item1 + i, CurrentTilePosition.Item2 + i)];
+                if (topRightPiece != null && !topRightBlocked)
+                {
+                    if (topRightPiece.PieceColor != PieceColor)
+                    {
+                        possibleMoves.Add((CurrentTilePosition.Item1 + i, CurrentTilePosition.Item2 + i));
+                        topRightBlocked = true;
+                    }
+                    else
+                    {
+                        topRightBlocked = true;
+                    }
+                }
+                else if (!topRightBlocked)
+                {
+                    possibleMoves.Add((CurrentTilePosition.Item1 + i, CurrentTilePosition.Item2 + i));
+                }
+            }
+            else
+            {
+                topRightBlocked = true;
+            }
+
+            // Bottom left diagonal moves
+            if (CurrentTilePosition.Item1 - i >= 0 && CurrentTilePosition.Item1 - i <= 7 && CurrentTilePosition.Item2 - i >= 0 && CurrentTilePosition.Item2 - i <= 7)
+            {
+                var bottomLeftPiece = pieceCoordsMap[(CurrentTilePosition.Item1 - i, CurrentTilePosition.Item2 - i)];
+                if (bottomLeftPiece != null && !bottomLeftBlocked)
+                {
+                    if (bottomLeftPiece.PieceColor != PieceColor)
+                    {
+                        possibleMoves.Add((CurrentTilePosition.Item1 - i, CurrentTilePosition.Item2 - i));
+                        bottomLeftBlocked = true;
+                    }
+                    else
+                    {
+                        bottomLeftBlocked = true;
+                    }
+                }
+                else if (!bottomLeftBlocked)
+                {
+                    possibleMoves.Add((CurrentTilePosition.Item1 - i, CurrentTilePosition.Item2 - i));
+                }
+            }
+            else
+            {
+                bottomLeftBlocked = true;
+            }
+
+
+            // Bottom right diagonal moves
+            if (CurrentTilePosition.Item1 + i >= 0 && CurrentTilePosition.Item1 + i <= 7 && CurrentTilePosition.Item2 - i >= 0 && CurrentTilePosition.Item2 - i <= 7)
+            {
+                var bottomRightPiece = pieceCoordsMap[(CurrentTilePosition.Item1 + i, CurrentTilePosition.Item2 - i)];
+                if (bottomRightPiece != null && !bottomRightBlocked)
+                {
+                    if (bottomRightPiece.PieceColor != PieceColor)
+                    {
+                        possibleMoves.Add((CurrentTilePosition.Item1 + i, CurrentTilePosition.Item2 - i));
+                        bottomRightBlocked = true;
+                    }
+                    else
+                    {
+                        bottomRightBlocked = true;
+                    }
+                }
+                else if (!bottomRightBlocked)
+                {
+                    possibleMoves.Add((CurrentTilePosition.Item1 + i, CurrentTilePosition.Item2 - i));
+                }
+            } 
+            else
+            {
+                bottomRightBlocked = true;
+            }
+
+
+            if (topLeftBlocked && topRightBlocked && bottomLeftBlocked && bottomRightBlocked)
+            {
+                break;
+            }
+        }
+
+        return possibleMoves;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
